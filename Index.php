0208d4f3fd3f2dfa2606e04845f95de0f5275fc9
yusseno <yusseno19@gmail.com>
2021-10-19 23:09:25 +0700

    205410050 yusseno index.php

--- Index.php
+++ Index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>coba (205410050)</h3>"
+echo "<h3>test (205410050)</h3>"
 ?>
